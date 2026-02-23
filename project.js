e44c89ec887b6136282a81f8a069e16c14d87ec4
wariz011 <adenikeadebara91@gmail.com>
2026-02-23 10:15:18 +0100

    update

---
+++ project.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const colorBox = document.getElementById("color-box");
+    const button = document.getElementById("change-color-btn");
+
+    // Function to generate random HEX color
+    function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+    // Change color when button is clicked
+    button.addEventListener("click", function () {
+        const newColor = getRandomColor();
+        colorBox.style.backgroundColor = newColor;
+    });
+
+});
